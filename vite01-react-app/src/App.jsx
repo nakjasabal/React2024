9044f72344de96fe0a10704effef3c0cbfcd4011
nakjasabal <nakjasabal@naver.com>
2024-09-26 12:09:53 +0900

    first commit

---
+++ vite01-react-app/src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+
+function FrontComp(props) {
+  const liRows = [];
+
+  for(let i=0 ; i<props.propData1.length ; i++){    
+    liRows.push(
+      <li key={i}>{props.propData1[i]}</li>
+    );
+  }
+
+  return (<>
+    <li>프론트앤드</li>
+    <ul>
+      {liRows}
+    </ul>
+  </>)
+}
+
+const BackComp = ({propData2}) => {
+  const liRows = [];
+
+  let keyCnt=0;
+  for(let row of propData2){
+    liRows.push(
+      <li key={keyCnt++}>{row}</li>
+    );
+  }
+
+  return (<>
+    <li>백앤드</li>
+    <ul>
+      {liRows}       
+    </ul>
+  </>)
+}
+
+function App() {
+  const frontData = ['HTML5', 'CSS3', 'Javascript', 'jQuery'];
+  const backData = ['Java', 'Oracle', 'JSP', 'Spring Boot'];
+
+  return (<>
+    <div>
+      <h2>React : Props</h2>
+      <ol>
+        <FrontComp propData1={frontData} ></FrontComp>
+        <BackComp propData2={backData} />
+      </ol>
+    </div>
+  </>)
+}
+
+export default App
