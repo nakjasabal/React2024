f9ee51cf265b52aab966ad6a2f6f485efd7b2812
nakjasabal <nakjasabal@naver.com>
2024-09-26 16:26:58 +0900

    React기초(완료)

--- vite01-react-app/src/App.jsx
+++ vite01-react-app/src/App.jsx
@@ -1,51 +1,40 @@
 import './App.css'
+import {useState} from 'react';
 
-function FrontComp(props) {
-  const liRows = [];
-
-  for(let i=0 ; i<props.propData1.length ; i++){    
-    liRows.push(
-      <li key={i}>{props.propData1[i]}</li>
-    );
-  }
-
+function WriteForm(props){
   return (<>
-    <li>프론트앤드</li>
-    <ul>
-      {liRows}
-    </ul>
-  </>)
-}
-
-const BackComp = ({propData2}) => {
-  const liRows = [];
-
-  let keyCnt=0;
-  for(let row of propData2){
-    liRows.push(
-      <li key={keyCnt++}>{row}</li>
-    );
-  }
-
-  return (<>
-    <li>백앤드</li>
-    <ul>
-      {liRows}       
-    </ul>
+    <form onSubmit={(e)=>{
+      console.log("이벤트객체e", e);
+      e.preventDefault();
+      let gubun = e.target.gubun.value;
+      let title = e.target.title.value;
+      props.writeAction(gubun, title);
+    }}>
+      <select name="gubun">
+        <option value="front">프론트앤드</option>
+        <option value="back">백앤드</option>
+      </select>
+      <input type="text" name="title" />
+      <input type="submit" value="추가" />
+    </form>
   </>)
 }
 
 function App() {
-  const frontData = ['HTML5', 'CSS3', 'Javascript', 'jQuery'];
-  const backData = ['Java', 'Oracle', 'JSP', 'Spring Boot'];
-
+  const [message, setMessage] = useState('폼값 검증 진행중');
   return (<>
     <div>
-      <h2>React : Props</h2>
-      <ol>
-        <FrontComp propData1={frontData} ></FrontComp>
-        <BackComp propData2={backData} />
-      </ol>
+      <h2>React : Form submit</h2>
+      <WriteForm writeAction={(gu, ti)=>{
+        console.log("Form값", gu, ti);
+        if(gu!=='' && ti!==''){
+          setMessage('폼값 검증 완료');
+        }
+        else{
+          alert("빈값있음");
+        }
+      }}/>
+      <p>{message}</p>
     </div>
   </>)
 }
